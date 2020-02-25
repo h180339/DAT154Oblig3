a219022cff117f02298f3c1c3b094ce3b559baab
eirikalv <h@g.gov>
2020-02-25 18:52:22 +0100

    added datagrid to desktop app

--- Oblig 3/Oblig 3 Desktop app/ManageReservation.xaml.cs
+++ Oblig 3/Oblig 3 Desktop app/ManageReservation.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DatabaseHandler;
+using DatabaseHandler.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +22,23 @@ namespace Oblig_3_Desktop_app
     /// </summary>
     public partial class ManageReservation : Page
     {
+        private BookingDbContext dbContext;
+
         public ManageReservation()
         {
             InitializeComponent();
+            Load();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new FrontPage());
+        }
+        private void Load()
+        {
+            dbContext = new BookingDbContext();
+            List<Reservation> resList = dbContext.Reservations.ToList();
+            reservationGrid.ItemsSource = resList;
         }
     }
 }
