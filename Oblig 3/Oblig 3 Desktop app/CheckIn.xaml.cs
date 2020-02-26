84a5afa4807c80b4ea6618d97454ddd4ab507054
Joakim Johesan <182719@stud.hvl.no>
2020-02-26 15:09:14 +0100

    Check in page fixed, it works?

--- Oblig 3/Oblig 3 Desktop app/CheckIn.xaml.cs
+++ Oblig 3/Oblig 3 Desktop app/CheckIn.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DatabaseHandler;
+using DatabaseHandler.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,15 +22,72 @@ namespace Oblig_3_Desktop_app
     /// </summary>
     public partial class CheckIn : Page
     {
+        private BookingDbContext dbContext;
+        private List<Reservation> resList;
         public CheckIn()
         {
             InitializeComponent();
+            Load();
+        }
+        private void Load()
+        {
+            dbContext = new BookingDbContext();
+            resList = dbContext.Reservations.ToList();
+            reservationGrid.ItemsSource = resList;
+            List<User> userList = dbContext.Users.ToList();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void HomeBtnClick(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new FrontPage());
 
+        }
+
+        private void checkInBtnClick(object sender, RoutedEventArgs e)
+        {
+            Reservation selectedReservation = reservationGrid.SelectedItem as Reservation;
+
+            Reservation databaseReservation = resList.Find(x => x.Id == selectedReservation.Id);
+
+            databaseReservation.Status = "Checked in";
+
+            dbContext.SaveChanges();
+            reservationGrid.ItemsSource = null;
+            Load();
+        }
+        private void checkOutBtnClick(object sender, RoutedEventArgs e)
+        {
+            Reservation selectedReservation = reservationGrid.SelectedItem as Reservation;
+
+            Reservation databaseReservation = resList.Find(x => x.Id == selectedReservation.Id);
+
+            databaseReservation.Status = "Checked out";
+
+            dbContext.SaveChanges();
+
+            reservationGrid.ItemsSource = null;
+            Load();
+
+
+        }
+
+        private void SearchBtnClick(object sender, RoutedEventArgs e)
+        {
+            List<Reservation> searchResList = dbContext.Reservations.ToList();
+
+            searchResList.RemoveAll(r => !(r.User.username.ToLower().Equals(searchText.Text.Trim().ToLower())));
+            reservationGrid.ItemsSource = null;
+            reservationGrid.ItemsSource = searchResList;
+        }
+
+        private void resetReservationGrid()
+        {
+            reservationGrid.ItemsSource = null;
+            reservationGrid.ItemsSource = resList;
+        }
+        private void resetBtnClick(object sender, RoutedEventArgs e)
+        {
+            resetReservationGrid();
         }
     }
 }
